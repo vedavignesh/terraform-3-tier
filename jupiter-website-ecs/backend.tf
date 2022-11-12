2fcffe61b31d8c22acd6be02fc9549c10e988083
vedavignesh <Veda@2021>
2022-11-12 17:22:05 +0530

    terraform-code-1

--- jupiter-website-ecs/backend.tf
+++ jupiter-website-ecs/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket    = "terraform-backend12-11-2022"
     key       = "jupiter-website-ecs.tfstate"
-    region    = "us-east-"
+    region    = "us-east-1"
     profile   = "cloud_user"
   }
 }
