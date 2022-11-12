83fa45cbaff2f98680ae1ccda93595a67f2905ba
vedavignesh <Veda@2021>
2022-11-12 17:21:41 +0530

    terr

--- jupiter-website-ecs/backend.tf
+++ jupiter-website-ecs/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket    = "terraform-backend12-11-2022"
     key       = "jupiter-website-ecs.tfstate"
-    region    = "us-east-1"
+    region    = "us-east-"
     profile   = "cloud_user"
   }
 }
